8c334a17833d01d540ee562879c2bc7cea5cd1ec
vektorel <vektorel@lab8-4>
2017-02-19 16:40:16 +0200

    thread

---
+++ FirsProject/src/com/vektorel2/thread/sayi/Islem.java
@@ -0,0 +1,22 @@
+package com.vektorel2.thread.sayi;
+
+public class Islem {
+
+	public static void main(String[] args) {
+		Sayi1 sayi1 = new Sayi1();
+		Sayi2 sayi2 = new Sayi2();
+		
+		Thread isParcacigi1 = new Thread(sayi1);
+		Thread isParcacigi2 = new Thread(sayi2);
+
+		isParcacigi1.start();
+		System.out.println("alt satýr");
+		
+		isParcacigi2.start();
+		
+		isParcacigi1.interrupt();
+		
+		
+		
+	}
+}
